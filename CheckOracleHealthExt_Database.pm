5b3a9db310fc330a65a1110ad5c3510ab74b90e8
myorama <florent.jardin@gmail.com>
2018-07-09 10:55:48 +0200

    Evol on Database and Job extensions

--- CheckOracleHealthExt_Database.pm
+++ CheckOracleHealthExt_Database.pm
@@ -11,7 +11,7 @@ sub init {
   if ($params{mode} =~ /my::database::size/) {
     @results = $self->{handle}->fetchall_array(q{
       SELECT NVL(type, 'TOTAL') type, tablespace_name, SUM(bytes_used) real_bytes_used,
-       SUM(real_bytes_free) real_bytes_free, SUM(real_bytes_max) real_bytes_max
+       SUM(bytes_free) bytes_free, SUM(real_bytes_max) real_bytes_max
       FROM (
         SELECT
              CASE WHEN a.tablespace_name IN ('SYSTEM','SYSAUX')
@@ -20,8 +20,6 @@ sub init {
              a.bytes, a.maxbytes bytes_max,
              a.bytes - c.bytes_free bytes_used, 
              c.bytes_free + NVL(d.bytes_expired, 0) bytes_free,
-             CASE WHEN a.maxbytes = 0 OR a.maxbytes < a.bytes THEN c.bytes_free 
-              ELSE c.bytes_free + (a.maxbytes - a.bytes) END real_bytes_free,
              CASE WHEN a.maxbytes = 0 OR a.maxbytes < a.bytes THEN a.bytes
               ELSE a.maxbytes END real_bytes_max
          FROM (
@@ -71,20 +69,18 @@ sub nagios {
     
     foreach (values %{$self->{results}}) {
       my($type, $name, $used, $free, $max) = @{$_};
-	  my $status = $self->check_thresholds($used / $max * 100, 90, 98);
-	  
-	  if ($status > 0 and defined $name) {
-	    $self->add_nagios($status, sprintf "tablespace %s is %.2f%%", $name, $used / $max * 100);
-	  } elsif (! defined $name) {
-        $self->add_perfdata(sprintf "\'%s_size\'=%.2f%s", lc $type, $max / $factor, $units);
-        $self->add_perfdata(sprintf "\'%s_usage\'=%.2f%s", lc $type, $used / $factor, $units);
-	  }
-	  
+      my $status = $self->check_thresholds($used / $max * 100, 90, 98);
+    
+      if ($status > 0 and defined $name) {
+        $self->add_nagios($status, sprintf "tablespace %s is %.2f%%", $name, $used / $max * 100);
+      }
       if ($type eq "TOTAL") {
         $self->add_nagios_ok(sprintf(
           "Global database size is %.2f%s (%.2f%% full)",
           $used / $factor, $units, $used / $max * 100
         ));
+        $self->add_perfdata(sprintf "\'%s_used\'=%.2f%s", lc $type, $used / $factor, $units);
+        $self->add_perfdata(sprintf "\'%s_allocated\'=%.2f%s", lc $type, ($used + $free) / $factor, $units);
       }
     }
   } elsif ($params{mode} =~ /my::database::activity/) {
--- CheckOracleHealthExt_Job.pm
+++ CheckOracleHealthExt_Job.pm
@@ -11,7 +11,8 @@ sub init {
       select 
         schema_user, what, to_char(last_date, 'YYYY/MM/DD HH24:MI:SS'), 
         failures, case broken when 'Y' then 1 else 0 end broken,
-        trunc(24 * 60 * 60 * (sysdate - last_date)) as sec_gap 
+        trunc(24 * 60 * 60 * (sysdate - last_date)) as sec_gap,
+        trunc(24 * (sysdate - next_date)) as hour_late
       from dba_jobs where lower(what) like '%dbms_refresh.refresh%'
     });
     my $count = 0;
@@ -29,7 +30,7 @@ sub nagios {
   
   if ($params{mode} =~ /my::job::refresh/) {    
     foreach (values %{$self->{results}}) {
-      my($schema, $what, $last_executed, $failures, $broken, $sec_gap) = @{$_};
+      my($schema, $what, $last_executed, $failures, $broken, $sec_gap, $hour_late) = @{$_};
       
       # grap materialized view name from "dbms_refresh.refresh" syntax
       $what =~ /refresh\('([^\(]+)'\)/;
@@ -37,21 +38,22 @@ sub nagios {
       
       if ($broken > 0) {
         $self->add_nagios_critical(sprintf "Job is broken since %s for %s view", $last_executed, $view);
-		$errors++;
+        $errors++;
+      } elsif ($hour_late >= 6) {
+        $self->add_nagios_critical(sprintf "Job is running since %s hours for %s view but not broken.", $hour_late, $view);
       } else {
         my $status = $self->check_thresholds($failures, 5, 15);
         $self->add_nagios($status, sprintf "Last job execution for %s view : %s (%d attemps)", $view, $last_executed, $failures);
-		$errors++ if ($status > 0);
+        $errors++ if ($status > 0);
       }
     }
     
-    # remove unnecessary messages from running jobs
-    $self->{nagios}->{messages}->{$ERRORS{OK}} = ();
-    
     # test if "results" hash contains no row
     if ((scalar keys %{$self->{results}}) == 0) {
-      $self->add_nagios_ok("No refresh job has been configured");
+      $self->add_nagios_warning("No refresh job has been configured");
     } elsif ($errors == 0) {
+      # remove unnecessary messages from running jobs
+      $self->{nagios}->{messages}->{$ERRORS{OK}} = ();
       $self->add_nagios_ok("All refresh jobs are running well")
     }
   }
